df69f88eecc8e35770ddecb839b4baa599ffcc79
Maxim Chechulnikov <ilfirin.nar@gmail.com>
2020-05-17 20:53:39 +0300

    plenty of scales

--- src/src/Scale.elm
+++ src/src/Scale.elm
@@ -6,21 +6,70 @@ import Note exposing (Note)
 
 type Scale
     = MajorPentatonicScale Note
+    | SuspendedPentatonicScale Note
+    | BluesMinorPentatonicScale Note
+    | BluesMajorPentatonicScale Note
     | MinorPentatonicScale Note
+    | IonianScale Note
+    | DorianScale Note
+    | PhrygianScale Note
+    | LydianScale Note
+    | MyxolydianScale Note
+    | AeolianScale Note
+    | LocrianScale Note
 
 
 toNotes : Scale -> List Note
 toNotes scale =
+    let getNotes tonic intervals = intervals |> Interval.structure tonic |> .notes in
     case scale of
         MajorPentatonicScale tonic ->
             [ MajorSecond, MajorSecond, MinorThird, MajorSecond, MinorThird ]
-                |> Interval.structure tonic
-                |> .notes
+                |> getNotes tonic
+
+        SuspendedPentatonicScale tonic ->
+            [ MajorSecond, MinorThird, MajorSecond, MinorThird, MajorSecond ]
+                |> getNotes tonic
+
+        BluesMinorPentatonicScale tonic ->
+            [ MinorThird, MajorSecond, MinorThird, MajorSecond, MajorSecond ]
+                |> getNotes tonic
+
+        BluesMajorPentatonicScale tonic ->
+            [ MajorSecond, MinorThird, MajorSecond, MajorSecond, MinorThird ]
+                |> getNotes tonic
 
         MinorPentatonicScale tonic ->
             [ MinorThird, MajorSecond, MajorSecond, MinorThird, MajorSecond ]
-                |> Interval.structure tonic
-                |> .notes
+                |> getNotes tonic
+
+        IonianScale tonic ->
+            [ MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond, MajorSecond, MinorSecond ]
+                |> getNotes tonic
+
+        DorianScale tonic ->
+            [ MajorSecond, MinorSecond, MajorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond ]
+                |> getNotes tonic
+
+        PhrygianScale tonic ->
+            [ MinorSecond, MajorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond ]
+                |> getNotes tonic
+
+        LydianScale tonic ->
+            [ MajorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond, MinorSecond ]
+                |> getNotes tonic
+
+        MyxolydianScale tonic ->
+            [ MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond ]
+                |> getNotes tonic
+
+        AeolianScale tonic ->
+            [ MajorSecond, MinorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond ]
+                |> getNotes tonic
+
+        LocrianScale tonic ->
+            [ MinorSecond, MajorSecond, MajorSecond, MinorSecond, MajorSecond, MajorSecond, MajorSecond ]
+                |> getNotes tonic
 
 
 toKindString : Scale -> String
@@ -29,8 +78,38 @@ toKindString scale =
         MajorPentatonicScale _ ->
             "major pentatonic"
 
+        SuspendedPentatonicScale _ ->
+            "suspended pentatonic"
+
+        BluesMinorPentatonicScale _ ->
+            "blues minor pentatonic"
+
+        BluesMajorPentatonicScale _ ->
+            "blues major pentatonic"
+
         MinorPentatonicScale _ ->
             "minor pentatonic"
+
+        IonianScale _ ->
+            "ionian"
+
+        DorianScale _ ->
+            "dorian"
+
+        PhrygianScale _ ->
+            "phrygian"
+
+        LydianScale _ ->
+            "lydian"
+
+        MyxolydianScale _ ->
+            "myxolydian"
+
+        AeolianScale _ ->
+            "aeolian"
+
+        LocrianScale _ ->
+            "locrian"
 
 
 fromKindString : String -> Maybe (Note -> Scale)
@@ -39,8 +118,38 @@ fromKindString val =
         "major pentatonic" ->
             Just MajorPentatonicScale
 
+        "suspended pentatonic" ->
+            Just SuspendedPentatonicScale
+
+        "blues minor pentatonic" ->
+            Just BluesMinorPentatonicScale
+
+        "blues major pentatonic" ->
+            Just BluesMajorPentatonicScale
+
         "minor pentatonic" ->
             Just MinorPentatonicScale
+
+        "ionian" ->
+            Just IonianScale
+
+        "dorian" ->
+            Just DorianScale
+
+        "phrygian" ->
+            Just PhrygianScale
+
+        "lydian" ->
+            Just LydianScale
+
+        "myxolydian" ->
+            Just MyxolydianScale
+
+        "aeolian" ->
+            Just AeolianScale
+
+        "locrian" ->
+            Just LocrianScale
 
         _ ->
             Nothing
